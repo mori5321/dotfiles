b2ca4b6919f95a54a75eea61048fb046bae3d7b0
mori5321 <moriwm77@gmail.com>
2022-09-06 00:10:02 +0900

    config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 bufOpen = {"BufNewFile", "BufRead"}
 
--- Temporary Runtimepath
-vim.opt.runtimepath:append {"$HOME/dev/projects/nvim-remaster/.config/nvim"}
+-- Runtimepath
+vim.opt.runtimepath:append {"$HOME/dotfiles/nvim"}
 
 -- Lazy Redraw
 vim.o.lazyredraw = true
@@ -25,20 +25,31 @@ vim.o.backspace = 'indent,eol,start'
 vim.o.clipboard = "unnamedplus"
 
 
+
 -- 行番号
 vim.o.number = true
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern="*",
-  callback = function()
-    vim.api.nvim_set_hl(0, "LineNr", { cterm=499, bold=true })   
-  end,
-})
-
--- vim.api.nvim_create_autocmd(bufOpen, {
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+--   pattern="*",
 --   callback = function()
---     vim.api.nvim_exec("ColorScheme * highlight LineNr ctermfg=499")
---   end
+--     vim.api.nvim_set_hl(0, "LineNr", { cterm = 499, bold = true })   
+--   end,
 -- })
+
+-- Color Scheme
+vim.opt.background = "dark"
+vim.cmd [[ colorscheme hybrid ]]
+
+-- Transparency
+for key, value in pairs({ "Normal", "NonText", "LineNr", "Folded", "EndOfBuffer" }) do
+  vim.api.nvim_set_hl(0, value, { ctermbg="none"})
+end
+-- vim.api.nvim_set_hl(0, "Normal", { ctermbg="none" })
+-- vim.api.nvim_set_hl(0, "NonText", { ctermbg="none"})
+-- highlight NonText ctermbg=none
+-- highlight LineNr ctermbg=none
+-- highlight Folded ctermbg=none
+-- highlight EndOfBuffer ctermbg=none
+
 
 -- Ftplugin
 mk_tab_size = function(size)
@@ -71,4 +82,91 @@ map_key('n', 'p', '<CR>zz<C-w>p')
 
 -- Packer.nvim
 require 'plugins'
-vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
+vim.cmd[[autocmd BufWritePost lua/plugins.lua PackerCompile]]
+
+
+-- fzf-lua
+vim.api.nvim_set_keymap('n', '<c-P>', "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true })
+
+
+
+-- bultin-lsp
+--
+-- SEE: https://zenn.dev/nazo6/articles/c2f16b07798bab
+-- SEE: https://github.com/neovim/nvim-lspconfig
+-- SEE: https://github.com/williamboman/mason.nvim
+local on_attach = function(client, bufnr)
+  local set = vim.keymap.set
+  
+  set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+  set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  set("n", "<space>h", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+
+  set("n", "<C-k>", "<cmd>lua vm.lsp.buf.signature_help()<CR>")
+
+  set("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
+  set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
+  set("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
+  set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+  set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+  set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+  set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+  set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>")
+  set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+end
+
+-- mason (external dependency installer for lsp)
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function (server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach
+    }
+  end,
+}
+
+
+-- nvim-cmp
+-- SEE: https://zenn.dev/nazo6/articles/c2f16b07798bab
+capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+vim.opt.completeopt = "menu,menuone,noselect"
+
+local cmp = require "cmp"
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+  }, {
+    { name = "buffer" },
+  })
+})
+
+
+-- caw
+-- "Ctrl-k => Single-Line Comment Out
+-- "Ctrl-c => Wrapped Comment Out
+map_key('n', '<C-k>', '<Plug>(caw:hatpos:toggle)')
+map_key('v', '<C-k>', '<Plug>(caw:hatpos:toggle)')
+map_key('n', '<C-c>', '<Plug>(caw:wrap:toggle)')
+map_key('v', '<C-c>', '<Plug>(caw:wrap:toggle)')
+
+
+
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,15 +2,50 @@ vim.cmd[[packadd packer.nvim]]
 
 -- SEE: https://qiita.com/delphinus/items/8160d884d415d7425fcc
 
+-- Commands
+-- :PackerInstall
+--
+-- :PackerCompile
+-- 
+-- :PackerUpdate
+-- 
+-- :PackerSync
+--
+-- :PackerClean
+
 require'packer'.startup(function()
-  use'tpope/vim-fugitive'
-  use'tpope/vim-repeat'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-repeat'
 
-  use{'wbthomason/packer.nvim', opt = true}
+  -- Package Manager
+  use { 'wbthomason/packer.nvim', opt = true }
 
-  use{'rhysd/git-messenger.vim', opt = true, cmd = {'GitMessenger'}}
+  -- Git
+  use { 'rhysd/git-messenger.vim', opt = true, cmd = {'GitMessenger'} }
 
-  use{'tpope/vim-unimpaired', opt = true, event = {'FocusLost', 'CursorHold'}}
+  use { 'tpope/vim-unimpaired', opt = true, event = {'FocusLost', 'CursorHold'} }
 
-  use{'fatih/vim-go', opt = true, ft = {'go'}}
+  use { 'fatih/vim-go', opt = true, ft = {'go'} }
+
+  -- Fuzzy Finder
+  use { 'ibhagwan/fzf-lua',
+    -- optional for icon support
+    requires = {'kyazdani42/nvim-web-devicons'}
+  }
+
+  
+  -- lsp
+  use "neovim/nvim-lspconfig"
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
+
+  -- cmp
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-vsnip"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/vim-vsnip"
+
+  -- comment out
+  use 'tyru/caw.vim'
 end)
